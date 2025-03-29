9f2d0026206de4b90187abb3fa6288768e182ce0
HarshitaV123 <100452399+HarshitaV123@users.noreply.github.com>
2025-03-29 12:57:35 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -40,4 +40,55 @@ class Bird(pygame.sprite.Sprite):
         self.images = []
         self.index = 0
         self.counter = 0
+        for num in range(1,4):
+            img = pygame.image.load(f"images/bird{num}.png")
+            self.images.append(img)
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.center = (x,y)
+        self.vel = 0
+        self.clicked = False
+
+    def update(self):
+        if flying == True:
+            self.vel += 0.5
+            if self.vel > 8:
+                self.vel = 8
+            if self.rect.bottom < 768:
+                self.rect.y += int(self.vel)
         
+        if game_over == False:
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                self.vel = -10
+            if pygame.mouse.get_pressed()[0] == 0:
+                self.clicked = False
+            
+            #handling the animation
+            flap_cooldown = 5
+            self.counter += 1
+
+            if self.counter > flap_cooldown:
+                self.counter = 0
+                self.index += 1
+                if self.index >= len(self.images):
+                    self.index = 0
+                self.image = self.images[self.index]
+            
+            #rotate the bird
+            self.image = pygame.transform.rotate(self.images[self.index],self.vel*-2)
+        else:
+            self.image = pygame.transform.rotate(self.images[self.index],-90)
+
+class Pipe(pygame.sprite.Sprite):
+    def __init__(self, x,y,position):
+        super().__init__(self)
+        self.image = pygame.image.load("images/pipe.png")
+        self.rect = self.image.get_rect()
+        if position == 1:
+            self.image = pygame.transform.flip(self.image, False, True)
+            self.rect.bottomleft = [x,y - int(pipe_gap/2)]
+            
+
+
+
